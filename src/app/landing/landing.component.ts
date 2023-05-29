b441555883b07cfd1d8d2f57f7e882ff7b9685ae
kumibrr <isra.lora99@gmail.com>
2023-05-29 19:44:18 +0200

    Added user auth

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,13 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { UserService } from '../user.service';
+import { noop, tap } from 'rxjs';
 
 @Component({
   selector: 'app-landing',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './landing.component.html',
-  styleUrls: ['./landing.component.css']
+  styleUrls: ['./landing.component.css'],
 })
 export class LandingComponent {
-
+  private userService = inject(UserService);
+  signIn() {
+    this.userService.signIn().pipe(tap(console.log)).subscribe(noop);
+  }
 }
--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,9 +1,24 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
+export interface User {
+  id: number;
+  key: string;
+  hasPhoto: true;
+  photo: 'https://www.infojobs.net/candidato.foto?id_candidato=eecd09b6-0cd1-4d22-bbea-70ee5aa57e10';
+  name: string;
+  surname1: string;
+  surname2: string;
+  validatedMail: 1;
+}
+
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
   constructor(private http: HttpClient) {}
+
+  signIn() {
+    return this.http.get<User>('https://api.infojobs.net/api/6/candidate');
+  }
 }
